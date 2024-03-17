7d55b67c91ce335f0979ce36586496723e130b72
forkball <erosdipede@gmail.com>
2024-03-17 15:46:25 -0400

    Update Contact title

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,18 @@
+import Button from "../components/Button";
+
 export default function Contact() {
   return (
-    <main className="flex min-h-screen flex-col justify-between">
-      <div className="mt-12 p-4">
-        <h1 className="flex flex-col text-4xl font-bebas">
-          LET&apos;S GET TOGETHER
-        </h1>
+    <main className="flex min-h-screen flex-col items-center w-full">
+      <div className="content w-full flex space-between mt-36 items-center mb-8">
+        <div className="p-4 mr-auto">
+          <h1 className="flex flex-col text-8xl font-bebas">LET&apos;S GET TOGETHER</h1>
+          <h2 className="text-4xl flex flex-col mb-4 font-bebas">
+            I&apos;M ALWAYS INTERESTED IN WORKING WITH OTHER ARTISTS
+          </h2>
+        </div>
+      </div>
+      <div className="content w-full">
+        <Button label="SEE WHAT I'M UP TO" />
       </div>
     </main>
   );
