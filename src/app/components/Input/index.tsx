35a510b2784784ec8b037dedbdd67ca17864046d
forkball <erosdipede@gmail.com>
2024-03-19 23:57:43 -0400

    Add input fields to contact page

--- src/app/components/Input/index.tsx
+++ src/app/components/Input/index.tsx
@@ -1,3 +1,20 @@
-export default function Input () { 
-    return <div></div>
+interface Props extends React.ComponentPropsWithoutRef<'input'> {
+  label: string;
+  value: string;
+  className?: string;
+}
+
+export default function Input({ label, value, className, onChange, placeholder }: Props)  {
+  return (
+    <div className={`flex flex-col grow ${className}`}>
+      <span className="font-bebas">{label}</span>
+      <input
+        type="text"
+        className="bg-secondary rounded text-white p-4 font-bebas"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import Button from "../components/Button";
 
 export default function Introduction() {
   return (
-    <main className="flex min-h-screen flex-col items-center w-full">
-      <div className="content w-full flex space-between mt-36 items-center mb-8">
-        <div className="w-72 p-4 mr-auto">
+    <main className="flex min-h-screen flex-col items-center w-full bg-primary">
+      <div
+        className="content w-full flex flex-col md:flex-row space-between items-center \
+                   mt-24 px-4 pb-12 gap-12"
+      >
+        <div className="w-full md:w-60 md:mr-auto text-left">
           <h1 className="flex flex-col text-8xl font-bebas">MALONE</h1>
           <h2 className="text-4xl flex flex-col mb-4 font-bebas">
             THE MAN THE MYTH <span>THE CHEMIST</span>
           </h2>
-          <p className="font-bebas text-xl">
+          <p className="text-xl font-bebas">
             Malone, a rising force in the underground rap scene, hails from the
             vibrant streets of Mississauga, where his lyrical prowess and unique
             sound have captivated audiences far and wide.
@@ -21,10 +23,18 @@ export default function Introduction() {
           alt="the chemist pondering"
           width={540}
           height={340}
+          className="w-full md:w-3/5 opacity-75 rounded"
         />
       </div>
-      <div className="content w-full">
-        <Button label="SEE WHAT I'M UP TO" />
+      <div className="border-b border-black w-full"></div>
+      <div className="content flex flex-col h-96 w-full px-4 pt-12">
+        <h2 className="font-bebas text-4xl text-white mb-4">LISTEN TO ME</h2>
+        <ol className="flex flex-col gap-2 text-black font-bebas text-2xl">
+          <li>SPOTIFY</li>
+          <li>APPLE MUSIC</li>
+          <li>SOUNDCLOUD</li>
+          <li>YOUTUBE</li>
+        </ol>
       </div>
     </main>
   );
