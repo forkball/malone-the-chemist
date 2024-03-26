05dc870757393c02d333e4d301424cc888730a3b
forkball <erosdipede@gmail.com>
2024-03-25 20:47:44 -0400

    Add Credit page

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <div className="bg-primary p-2 flex gap-8 font-bebas border-t border-black justify-center z-10">
       <span>(c) Copyright MALONE THE CHEMIST 2024. All rights reserved.</span>
+      <a href="/credits">credits</a>
     </div>
   );
 }
---
+++ src/app/credits/page.tsx
@@ -0,0 +1,17 @@
+export default function Credits() {
+  return (
+    <main className="flex min-h-screen flex-col items-center w-full gap-4 bg-primary">
+      <div className="content w-full flex flex-col space-between mt-24 items-center mb-8 px-4">
+        <div className="mr-auto">
+          <h1 className="flex flex-col text-8xl font-bebas">CREDITS</h1>
+        </div>
+        <div className="flex justify-start w-full">
+          <div className="border">
+            <h2>STARZZZ</h2>
+            <p>Malone the Chemist</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
